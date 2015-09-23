8b02bbe27b6891a22a095dce565703768bfa628d
RDanilek <rogerdanilek@hanovernorwichschools.org>
2015-09-23 13:51:16 -0400

    Updated from Brython Server: September 23, 2015 1:51:16 PM GMT-4

--- distribution.py
+++ distribution.py
@@ -36,9 +36,7 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
-import string
-a = input("Please enter a string of text (the bigger the better): ")
-out = a.translate(string.maketrans("",""), string.punctuation)
 
-#(a.lower())
+a = input("Please enter a string of text (the bigger the better): ")
+print(a.lower())
 #print("The distribution of characters in "+ a +" is: ")
