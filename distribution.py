d09537d912bd46fddaf71c4edcfc02a030dde826
RDanilek <rogerdanilek@hanovernorwichschools.org>
2015-09-28 14:50:10 -0400

    Updated from Brython Server: September 28, 2015 2:50:10 PM GMT-4

--- distribution.py
+++ distribution.py
@@ -38,8 +38,11 @@ Notice about this example:
 """
 import string
 string.ascii_lowercase
+
 a = input("Please enter a string of text (the bigger the better): ")
+
 print(a.split().sort())
+zip(a,string)
 
 
 #print("The distribution of characters in "+ a +" is: ")
