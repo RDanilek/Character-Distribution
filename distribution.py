d9796a3c2daaa09d6532c8338b31f50cbababef0
RDanilek <rogerdanilek@hanovernorwichschools.org>
2015-10-02 14:53:12 -0400

    Updated from Brython Server: October 2, 2015 2:53:13 PM GMT-4

--- distribution.py
+++ distribution.py
@@ -44,7 +44,7 @@ b = input("Please enter a string of text (the bigger the better): ")
 print('The distribution of characters in "'+ b +'" is: ')
 
 counts = []
-a.lower()
+b.lower()
 for i in list(a):
     counts.append(b.count(i))
 zipper = list(zip(counts,a))
