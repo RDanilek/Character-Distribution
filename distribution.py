d7f31e7f11f97e52ba97c37452971abf8956a980
RDanilek <rogerdanilek@hanovernorwichschools.org>
2015-10-02 14:39:24 -0400

    Updated from Brython Server: October 2, 2015 2:39:25 PM GMT-4

--- distribution.py
+++ distribution.py
@@ -47,7 +47,7 @@ counts = []
 for i in list(a):
     counts.append(b.count(i))
 zipper = list(zip(counts,a)
-zipper.sort(lamba.f: -f[1],f[0])
+zipper.sort(lambda f: -f[1],f[0])
 for h in zipper:
     if h[0]!=0:
         print(h[0]*h[1])
